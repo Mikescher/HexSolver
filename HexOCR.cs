98a3690ab564eb1e2bfe5765565521a3c47c953c
Mike Schwörer <mailport@mikescher.de>
2015-04-12 04:06:19 +0200

    Draw Hexagons

--- HexOCR.cs
+++ HexOCR.cs
@@ -1,12 +1,75 @@
-﻿using System.Drawing;
+﻿using MSHC.Geometry;
+using MSHC.Helper;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 
 namespace HexSolver
 {
 	class HexOCR
 	{
+		public double CellRadius = 20;
+		public double CellGap = 5;
+		public double CorrectionHorizontal = 0;
+		public double CorrectionVertical = 0;
+		public double PaddingY = 0;
+		public double PaddingX = 0;
+		public double NoCellBarRight = 0;
+		public bool InitialSwap = true;
+
 		public Bitmap FindCells(Bitmap shot)
 		{
+			shot = new Bitmap(shot);
+
+			using (Graphics g = Graphics.FromImage(shot))
+			{
+				Pen pen = new Pen(new SolidBrush(Color.Magenta));
+
+				foreach (var hex in GetAllHexagons(shot))
+				{
+					var points = Enumerable.Range(0, 7).Select(p => hex.GetEdge(p)).Select(p => new Point((int)p.X, (int)p.Y)).ToArray();
+
+					g.FillPolygon(new SolidBrush(Color.FromArgb(32, 255, 0, 255)), points, System.Drawing.Drawing2D.FillMode.Alternate);
+					g.DrawLines(pen, points);
+				}
+			}
+
 			return shot;
+		}
+
+		public IEnumerable<HexagonCell> GetAllHexagons(Bitmap shot)
+		{
+			double CellHeight = CellRadius * (Math.Sin(MathExt.ToRadians(60)) / Math.Sin(MathExt.ToRadians(90))); // Mittelpunkt zu Kante
+
+			double horzDistance = (CellHeight + CellGap + CellHeight) * (Math.Cos(MathExt.ToRadians(30)));
+			double vertDistance = CellHeight + CellGap + CellHeight;
+
+			horzDistance += CorrectionHorizontal;
+			vertDistance += CorrectionVertical;
+
+			double posx = PaddingX;
+			double posy = PaddingY;
+
+			bool shiftY = InitialSwap;
+			if (shiftY)
+				posy += CellHeight + CellGap / 2;
+
+			while (posx + CellRadius + CellGap + NoCellBarRight < shot.Width)
+			{
+				while (posy + CellRadius + CellGap < shot.Height)
+				{
+					yield return new HexagonCell(new Vec2d(posx, posy), CellRadius, shot);
+
+					posy += vertDistance;
+				}
+
+				posx += horzDistance;
+				posy = PaddingY;
+				shiftY = !shiftY;
+				if (shiftY)
+					posy += CellHeight + CellGap / 2;
+			}
 		}
 	}
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Interop;
 using System.Windows.Media.Imaging;
 
@@ -15,24 +16,64 @@ namespace HexSolver
 		[DllImport("gdi32")]
 		static extern int DeleteObject(IntPtr o);
 
+		private HexCam cam = null;
+		private HexOCR ocr = null;
+		private Bitmap screenshot = null;
+
+		private bool skipUpdate = false;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			cam = new HexCam();
+			ocr = new HexOCR();
 		}
 
 		private void onCaptureClicked(object sender, RoutedEventArgs e)
 		{
-			HexCam cam = new HexCam();
-			HexOCR ocr = new HexOCR();
+			if (cam == null)
+				return;
+			if (ocr == null)
+				return;
+			if (screenshot == null)
+				screenshot = cam.GetScreenShot();
 
-			Bitmap image = cam.GetScreenShot();
-			image = ocr.FindCells(image);
-
-			imgDisplay.Source = LoadBitmap(image);
-
+			imgDisplay.Source = LoadBitmap(ocr.FindCells(screenshot));
 		}
 
-		public static BitmapSource LoadBitmap(Bitmap source)
+		private void HexOCRValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+		{
+			HexOCRValueSet(sender, null);
+		}
+
+		private void cbSwap_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			HexOCRValueSet(sender, null);
+		}
+
+		private void HexOCRValueSet(object sender, RoutedEventArgs e)
+		{
+			if (cam == null)
+				return;
+			if (ocr == null)
+				return;
+			if (skipUpdate)
+				return;
+			if (screenshot == null)
+				screenshot = cam.GetScreenShot();
+
+			updateOCRProperties();
+
+			imgDisplay.Source = LoadBitmap(ocr.FindCells(screenshot));
+		}
+
+		private void HexOCRValueUpdate(object sender, RoutedEventArgs e)
+		{
+			updateOCRValues();
+		}
+
+		private static BitmapSource LoadBitmap(Bitmap source)
 		{
 			IntPtr ip = source.GetHbitmap();
 			BitmapSource bs = null;
@@ -46,6 +87,34 @@ namespace HexSolver
 			}
 
 			return bs;
+		}
+
+		private void updateOCRProperties()
+		{
+			ocr.CellRadius = dudRadius.Value.Value;
+			ocr.CellGap = dudGap.Value.Value;
+			ocr.CorrectionHorizontal = dudHCorr.Value.Value;
+			ocr.CorrectionVertical = dudVCorr.Value.Value;
+			ocr.PaddingX = dudPadX.Value.Value;
+			ocr.PaddingY = dudPadY.Value.Value;
+			ocr.InitialSwap = cbSwap.SelectedIndex == 0;
+			ocr.NoCellBarRight = dudBarRight.Value.Value;
+		}
+
+		private void updateOCRValues()
+		{
+			skipUpdate = true;
+
+			dudRadius.Value = ocr.CellRadius;
+			dudGap.Value = ocr.CellGap;
+			dudHCorr.Value = ocr.CorrectionHorizontal;
+			dudVCorr.Value = ocr.CorrectionVertical;
+			dudPadX.Value = ocr.PaddingX;
+			dudPadY.Value = ocr.PaddingY;
+			cbSwap.SelectedIndex = ocr.InitialSwap ? 0 : 1;
+			dudBarRight.Value = ocr.NoCellBarRight;
+
+			skipUpdate = false;
 		}
 	}
 }
